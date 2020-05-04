40e71f214d625eebb7f4d82d97448b449016dcb9
Tristan, Mario <mario.tristan@accenture.com>
2020-05-04 09:41:37 -0500

    Solution for problem 1 with c#

---
+++ Problem_1/c_sharp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace c_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            Int64 sum =0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i%3==0 || i%5==0)
+                {
+                    sum = +i;
+                }
+               
+
+            }
+        
+        Console.WriteLine(sum);
+            
+        }
+    }
+}
