f32808a06c73543f6df008ed5726e3b6ca6488b6
Mario Tristan <mariotristan@gmail.com>
2020-11-09 00:54:55 -0600

    solution for problem 4

---
+++ Problem_4/c_sharp/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace c_sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int larger = 0;
+            for (int i = 100; i < 1000; i++)
+            {
+                for (int x = 100; x < 1000; x++)
+                {
+                    int product = i*x;
+                    string products = product.ToString();
+
+                    char[] charArray = products.ToCharArray();
+                    Array.Reverse( charArray );
+                    string reverse = new string(charArray);     
+                //   Console.WriteLine(products + " - " + reverse);
+                    if (products == reverse)
+                    {
+                        if (product > larger)
+                        {
+                            larger =  product;
+                        }
+                        
+                          Console.WriteLine("{0} X {1} = {2} --- {3}",i,x,product, reverse);
+                    }
+
+                  
+                }
+            }
+            Console.WriteLine (larger);
+          
+        }
+    }
+}
